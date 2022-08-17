41d977d252c4931395ff369197cfa40b6ff44944
alimhp <alimohamadipourr@gmail.com>
2022-08-17 06:24:45 -0700

    ass route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
-import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import CartPage from "./pages/CartPage";
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <HomePage />
-      </div>
-    </BrowserRouter>
+    <Router>
+      <Switch>
+        <Route path="/cart" component={CartPage} />
+        <Route path="/" component={HomePage} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/pages/CartPage.js
@@ -0,0 +1,11 @@
+import Layout from "../layout/layout";
+
+const CartPage = () => {
+  return (
+    <Layout>
+      <h2>this is cart page</h2>
+    </Layout>
+  );
+};
+
+export default CartPage;
