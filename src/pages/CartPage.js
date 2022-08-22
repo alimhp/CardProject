77daea10381758b74d2d5f56c9983acf9af558f8
alimhp <alimohamadipourr@gmail.com>
2022-08-22 03:20:37 -0700

    Load cart products

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -1,11 +1,32 @@
+import { useCart } from "../Contex/CartProvider";
 import Layout from "../layout/layout";
 
 const CartPage = () => {
+  const { cart } = useCart();
+  // console.log(cart);
   return (
     <Layout>
-      <h2>this is cart page</h2>
+      <main>
+        {cart.length ? (
+          cart.map((item) => <div key={item.id}>{item.name}</div>)
+        ) : (
+          <p>no item in cart !...</p>
+        )}
+      </main>
     </Layout>
   );
 };
+
+// <main>
+// {cart.length ? (
+//     cart.map((item) => (
+// <div key={item.id}>
+//     <p>{item.name}</p>
+// </div>
+// ))
+// ) : (
+// <p>no item in cart !</p>
+// )}
+// </main>
 
 export default CartPage;
